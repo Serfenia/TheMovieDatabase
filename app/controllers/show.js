9a5a48748a3eccdbbc46615a99ba9b8b60409043
Patrick van Vuuren <patr_vv@hotmail.com>
2015-02-12 23:00:23 +0100

    added model check

--- app/controllers/show.js
+++ app/controllers/show.js
@@ -19,7 +19,7 @@ function clickTab(e) {
 function setActionBarTitle() {
 	var m = Alloy.createModel('movie');
 	m.fetch({id: args.id});
-	$.win.setTitle(m.toJSON()[0].original_title);
+	m.toJSON()[0] && $.win.setTitle(m.toJSON()[0].original_title);
 }
 
 function shareItem() {
